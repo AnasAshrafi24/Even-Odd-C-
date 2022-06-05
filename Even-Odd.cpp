f39d53c8550283735c5881159798905d826bd6de
AnasAshrafi24 <Anas24ashrafi@gmail.com>
2022-06-06 00:29:07 +0500

    Even-Odd C++

---
+++ Even-Odd.cpp
@@ -0,0 +1,80 @@
+#include<stdio.h>
+//int main () 
+//{
+//	int a,b,sum,sub,mul,div;
+//	printf("Enter first number\n");
+//	scanf("%d",&a);
+//	printf("Enter second number\n");
+//	scanf("%d",&b);
+//	sum=a+b;
+//	sub=a-b;
+//	mul=a*b;
+//	div=a/b;
+//	printf("\nthe sum of 2 number is %d",sum);
+//	printf("\nthe subtraction of 2 number is %d",sub);
+//	printf("\nthe multiplication of 2 number is %d",mul);
+//	printf("\nthe division of 2 number is %d",div);
+//}
+
+
+//int main ()
+//{
+//	int marks;
+//	printf("Enter your marks\n");
+//	scanf("%d",&marks);
+//	if(marks>=33)
+//	{
+//		printf("congratulation you are pass ");
+//		printf("Keep working hard\n");
+//	}
+//	else{
+//		printf("You have failed in this subject");
+//	}
+//}
+
+
+//Taking reminder
+//int main()
+//{
+//	int a = 32;
+//	int b = 2;
+//	int result = a%b;
+//	printf("The result of %d",result); 
+//}
+
+//int main ()
+//{
+//  int num;
+//  printf("Enter number to find even number or add\n");
+//  scanf("%d",&num);
+//  if(num%2==0)
+//  {
+//  	printf("\nThe number entered is even");
+//  }
+//  else
+//  {
+//  	printf("the reminder is odd");
+//  }
+//}
+
+
+int main ()
+{
+  int num;
+  printf("Enter number to find even number or add\n");
+  scanf("%d",&num);
+  if(num>0)
+  {
+  	printf("\nThe number entered is positive");
+  }
+  else
+  {
+  	printf("the reminder is negative");
+  }
+}
+
+
+
+
+
+
